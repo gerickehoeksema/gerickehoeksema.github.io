a66de7d8865d1b7f1e7d4e9344878e68b882d470
Gericke <gerickehoeksema@gmail.com>
2024-01-06 09:28:18 +0200

    Updated files

--- precache-manifest.30e345a1ab602ee3b21679d69b08bb9a.js
+++ precache-manifest.30e345a1ab602ee3b21679d69b08bb9a.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "c65a4afd4fa234a29d548e360926374a",
-    "url": "/profile/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "443637240facbcbb9de4",
-    "url": "/profile/static/css/main.70c3f86f.chunk.css"
+    "url": "/static/css/main.70c3f86f.chunk.css"
   },
   {
     "revision": "49111c0513e756841cef",
-    "url": "/profile/static/js/2.2642a3d9.chunk.js"
+    "url": "/static/js/2.2642a3d9.chunk.js"
   },
   {
     "revision": "443637240facbcbb9de4",
-    "url": "/profile/static/js/main.9ae5c9ad.chunk.js"
+    "url": "/static/js/main.9ae5c9ad.chunk.js"
   },
   {
     "revision": "189fd871db148202566e",
-    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+    "url": "/static/js/runtime~main.cc8686f1.js"
   }
 ]);
